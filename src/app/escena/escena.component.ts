8645ed79b06ea563d2db4472ee9fba4d4c4ae1d4
Paola Miramontes <pmiramonteso@gmail.com>
2024-10-02 21:11:33 +0200

    primer intento de animacion

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,13 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IStep } from '../interface/i-step';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+const slideInOut = [
+  transition(':enter', [
+    style({ transform: 'translateY(-100%)', opacity: 0 }),
+    animate('300ms ease-in', style({ transform: 'translateY(0)', opacity: 1 }))
+  ]),
+  transition(':leave', [
+    animate('300ms ease-out', style({ transform: 'translateY(-100%)', opacity: 0 }))
+  ])
+];
 
 @Component({
   selector: 'app-escena',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './escena.component.html',
-  styleUrls: ['./escena.component.scss']
+  styleUrls: ['./escena.component.scss'],
+  animations: [
+    trigger('slideInOut', slideInOut)
+  ]
 })
 export class EscenaComponent {
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,6 +18,5 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.steps = this.stepsService.getSteps();
-    console.log(this.steps);
   }
 }
