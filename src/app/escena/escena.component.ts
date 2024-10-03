790e0c4f70d6b7e486104d29671da0dea8ab01a8
Paola Miramontes <pmiramonteso@gmail.com>
2024-10-03 19:46:33 +0200

    ajustes animacion

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { IStep } from '../interface/i-step';
 import { trigger, transition, style, animate } from '@angular/animations';
 
-
 const slideInOut = [
   transition(':enter', [
     style({ transform: 'translateX(100%)', opacity: 0 }),
     animate('300ms ease-in', style({ transform: 'translateX(0)', opacity: 0 }))
   ]),
   transition(':leave', [
-    animate('300ms ease-out', style({ transform: 'translateX(100%)', opacity: 0 }))
+    animate('300ms ease-out', style({ transform: 'translateX(-100%)', opacity: 0 }))
   ])
 ];
+
 
 @Component({
   selector: 'app-escena',
