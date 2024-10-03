42bd4130be575202d7461a91755792fc40624a90
pmiramonteso <pmiramonteso@gmail.com>
2024-10-03 13:27:57 +0200

    animacion final

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IStep } from '../interface/i-step';
 import { trigger, transition, style, animate } from '@angular/animations';
 
+
 const slideInOut = [
   transition(':enter', [
-    style({ transform: 'translateY(-100%)', opacity: 0 }),
-    animate('300ms ease-in', style({ transform: 'translateY(0)', opacity: 1 }))
+    style({ transform: 'translateX(100%)', opacity: 0 }),
+    animate('300ms ease-in', style({ transform: 'translateX(0)', opacity: 0 }))
   ]),
   transition(':leave', [
-    animate('300ms ease-out', style({ transform: 'translateY(-100%)', opacity: 0 }))
+    animate('300ms ease-out', style({ transform: 'translateX(100%)', opacity: 0 }))
   ])
 ];
 
